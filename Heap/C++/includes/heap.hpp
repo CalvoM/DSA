ec3faba5483c0ef91611afc44c3f961eca650ffd
CalvoM <mwadimemakokha@gmail.com>
2023-02-02 21:07:58 +0300

    heap: max and min

--- Heap/C++/includes/heap.hpp
+++ Heap/C++/includes/heap.hpp
@@ -8,12 +8,19 @@ private:
   HeapType heapType;
   void maxCreateHeap();
   void minCreateHeap();
+  void maxDeleteHeap();
+  void minDeleteHeap();
+  int heap_size;
 
 public:
-  BHeap(HeapType heapType) { this->heapType = heapType; }
+  BHeap(HeapType heapType) {
+    this->heapType = heapType;
+    this->heap_size = 0;
+  }
   int AddElement(int value);
-  void Delete();
+  int Delete();
   void Print();
+  void Sort();
 };
 
 int BHeap::AddElement(int value) {
@@ -28,25 +35,11 @@ int BHeap::AddElement(int value) {
   default:
     break;
   }
-  return this->heap.size() - 1;
-}
-void BHeap::Delete() {
-  int del_el = this->heap[0];
-  this->heap[0] = this->heap[this->heap.size() - 1];
-  this->heap.pop_back();
-  switch (this->heapType) {
-  case HeapType::max:
-    this->maxCreateHeap();
-    break;
-  case HeapType::min:
-    this->minCreateHeap();
-    break;
-  default:
-    break;
-  }
+  this->heap_size++;
+  return this->heap_size - 1;
 }
 void BHeap::minCreateHeap() {
-  int new_el_index = this->heap.size() - 1;
+  int new_el_index = this->heap_size - 1;
   int parent_index = 0;
   while (new_el_index > 0) {
     parent_index = (new_el_index + 1) / 2 - 1;
@@ -61,7 +54,7 @@ void BHeap::minCreateHeap() {
   }
 }
 void BHeap::maxCreateHeap() {
-  int new_el_index = this->heap.size() - 1;
+  int new_el_index = this->heap_size - 1;
   int parent_index = 0;
   while (new_el_index > 0) {
     parent_index = (new_el_index + 1) / 2 - 1;
@@ -75,9 +68,74 @@ void BHeap::maxCreateHeap() {
     }
   }
 }
+int BHeap::Delete() {
+  int del_el = this->heap[0];
+  this->heap[0] = this->heap[this->heap_size - 1];
+  this->heap[this->heap_size - 1] = del_el;
+  this->heap_size--;
+  switch (this->heapType) {
+  case HeapType::max:
+    this->maxDeleteHeap();
+    break;
+  case HeapType::min:
+    this->minDeleteHeap();
+    break;
+  default:
+    break;
+  }
+  return del_el;
+}
+void BHeap::minDeleteHeap() {
+  int curr_index = 0;
+  int heap_index = curr_index + 1;
+  int lchild, rchild, temp;
+  while(curr_index < (this->heap_size - 1)) {
+    heap_index = curr_index + 1;
+    lchild = heap_index * 2;
+    rchild = lchild + 1;
+    if(this->heap[rchild -1] < this->heap[lchild -1]){
+      temp = this->heap[curr_index];
+      this->heap[curr_index] = this->heap[rchild -1];
+      this->heap[rchild - 1] = temp;
+      curr_index = rchild -1;
+    } else {
+      temp = this->heap[curr_index];
+      this->heap[curr_index] = this->heap[lchild -1];
+      this->heap[lchild - 1] = temp;
+      curr_index = lchild -1;
+    }
+  }
+}
+void BHeap::maxDeleteHeap() {
+  int curr_index = 0;
+  int heap_index = curr_index + 1;
+  int lchild, rchild, temp;
+  while(curr_index < (this->heap_size - 1)) {
+    heap_index = curr_index + 1;
+    lchild = heap_index * 2;
+    rchild = lchild + 1;
+    if(this->heap[rchild -1] > this->heap[lchild -1]){
+      temp = this->heap[curr_index];
+      this->heap[curr_index] = this->heap[rchild -1];
+      this->heap[rchild - 1] = temp;
+      curr_index = rchild -1;
+    } else {
+      temp = this->heap[curr_index];
+      this->heap[curr_index] = this->heap[lchild -1];
+      this->heap[lchild - 1] = temp;
+      curr_index = lchild -1;
+    }
+  }
+
+}
 void BHeap::Print() {
   for (int i : this->heap) {
     std::cout << i << " ";
   }
   std::cout << std::endl;
+}
+void BHeap::Sort() {
+  for (int i = 0; i < this->heap.size(); i++) {
+    this->Delete();
+  }
 }
--- Heap/C++/main.cpp
+++ Heap/C++/main.cpp
@@ -2,18 +2,14 @@
 
 int main(int argc, char **argv) {
   BHeap heap(HeapType::min);
-  heap.AddElement(3);
+  heap.AddElement(10);
+  heap.AddElement(20);
+  heap.AddElement(30);
+  heap.AddElement(25);
+  heap.AddElement(5);
+  heap.AddElement(40);
+  heap.AddElement(35);
   heap.Print();
-  heap.AddElement(1);
-  heap.Print();
-  heap.AddElement(7);
-  heap.Print();
-  heap.AddElement(2);
-  heap.Print();
-  heap.AddElement(9);
-  heap.Print();
-  heap.Delete();
-  heap.Print();
-  heap.Delete();
+  heap.Sort();
   heap.Print();
 }
