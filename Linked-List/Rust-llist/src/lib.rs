485ed289af7c38977cf70ceb8e760b9856078be8
CalvoM <mwadimemakokha@gmail.com>
2023-02-03 09:40:54 +0300

    llist: linked list begin

--- Linked-List/Rust-llist/src/lib.rs
+++ Linked-List/Rust-llist/src/lib.rs
@@ -1,3 +1,2 @@
-pub use node::Node;
+pub use node::{LinkedList, Node};
 mod node;
-
--- Linked-List/Rust-llist/src/node.rs
+++ Linked-List/Rust-llist/src/node.rs
@@ -1,11 +1,30 @@
 #[derive(Debug)]
 pub struct Node {
-    value: u64,
-    next: Option<Box<Node>>,
+    pub value: u64,
+    pub next: Option<Box<Node>>,
 }
 
 impl Node {
     pub fn new(value: u64, next: Option<Box<Node>>) -> Node {
         Node { value, next }
+    }
+}
+
+pub struct LinkedList {
+    head: Option<Box<Node>>,
+}
+
+impl LinkedList {
+    pub fn new() -> LinkedList {
+        LinkedList { head: None }
+    }
+
+    pub fn add_to_front(&mut self, value: u64) {
+        let node = Node::new(value, None);
+        if self.head.is_some() {
+            node.next = self.head;
+        } else {
+            self.head = Some(Box::new(node));
+        }
     }
 }
